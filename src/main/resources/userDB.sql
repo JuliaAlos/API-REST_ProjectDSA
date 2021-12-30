d3f18234235bbac9bf9891b5d163d991be2ea045
mirarnau <arnau.mir@estudiantat.upc.edu>
2021-12-30 14:45:21 +0100

    More data inserted

--- src/main/resources/userDB.sql
+++ src/main/resources/userDB.sql
@@ -68,10 +68,10 @@ CREATE TABLE Plane (
 
 INSERT INTO Player(PlayerName) VALUES ('pau player');
 INSERT INTO Player(PlayerName) VALUES ('julia player');
-INSERT INTO User(UserName, Password, PlayerId) VALUES ('Pau', MD5('Pau'),
-                                                       (SELECT id from Player WHERE PlayerName = 'pau player'));
-INSERT INTO User(UserName, Password, PlayerId) VALUES ('Julia', MD5('Julia'),
-                                             (SELECT id from Player WHERE PlayerName = 'julia player'));
+INSERT INTO Player(PlayerName) VALUES ('arnau player')
+INSERT INTO User(UserName, Password, PlayerId) VALUES ('Pau', MD5('Pau'),(SELECT id from Player WHERE PlayerName = 'pau player'));
+INSERT INTO User(UserName, Password, PlayerId) VALUES ('Arnau', MD5('Arnau'),(SELECT id from Player WHERE PlayerName = 'arnau player'));
+INSERT INTO User(UserName, Password, PlayerId) VALUES ('Julia', MD5('Julia'),(SELECT id from Player WHERE PlayerName = 'julia player'));
 
 INSERT INTO InsigniaModel(Name, Type) VALUES ('Insignia 1', 'Tipo1');
 INSERT INTO InsigniaModel(Name, Type) VALUES ('Insignia 2', 'Tipo2');
@@ -80,3 +80,6 @@ INSERT INTO InsigniaModel(Name, Type) VALUES ('Insignia 3', 'Tipo3');
 INSERT INTO PlaneModel(Model, Fuel, EnginesLife, VelX, VelY, Gravity) VALUES ('Airbus', 10, 3, 3, 1, 9.81);
 INSERT INTO PlaneModel(Model, Fuel, EnginesLife, VelX, VelY, Gravity) VALUES ('Fighter', 5, 1, 5, 5, 9.81);
 INSERT INTO PlaneModel(Model, Fuel, EnginesLife, VelX, VelY, Gravity) VALUES ('Cessna', 6, 2, 1, 2, 9.81);
+
+INSERT INTO Plane(PlayerId, PlaneModelModel) VALUES ((SELECT id from Player WHERE PlayerName = 'arnau player'), 'Airbus');
+INSERT INTO Plane(PlayerId, PlaneModelModel) VALUES ((SELECT id from Player WHERE PlayerName = 'arnau player'), 'Cessna');
