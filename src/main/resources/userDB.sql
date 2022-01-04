f895b10422ba08696c679aa5a7195fdb9b53e897
marcazlor <marc.azlor@estudiantat.upc.edu>
2022-01-04 13:16:22 +0100

    a

--- src/main/resources/userDB.sql
+++ src/main/resources/userDB.sql
@@ -33,9 +33,9 @@ CREATE TABLE InsigniaModel (
 CREATE TABLE Insignia (
                         Id varchar(255) PRIMARY KEY,
                         Name varchar(255) NOT NULL,
-                        FOREIGN KEY (Name) references InsigniaModel(Name),
                         Data varchar(255) DEFAULT 0 NOT NULL,
                         PlayerId varchar(255) NOT NULL,
+                        FOREIGN KEY (Name) references InsigniaModel(Name),
                         FOREIGN KEY (PlayerId) references Player(Id)
 );
 
@@ -68,11 +68,9 @@ CREATE TABLE Plane (
 INSERT INTO Player(PlayerName, Id) VALUES ('Pau', 'IdPau');
 INSERT INTO Player(PlayerName, Id) VALUES ('Julia', 'IdJulia');
 INSERT INTO Player(PlayerName, Id) VALUES ('Arnau', 'IdArnau');
-INSERT INTO Player(PlayerName, Id) VALUES ('Marc', 'IdMarc');
 INSERT INTO User(UserName, Password, PlayerId, Id) VALUES ('Pau', MD5('Pau'),(SELECT id from Player WHERE PlayerName = 'Pau'), 'A');
 INSERT INTO User(UserName, Password, PlayerId, Id) VALUES ('Arnau', MD5('Arnau'),(SELECT id from Player WHERE PlayerName = 'Arnau'),'B');
 INSERT INTO User(UserName, Password, PlayerId, Id) VALUES ('Julia', MD5('Julia'),(SELECT id from Player WHERE PlayerName = 'Julia'), 'C');
-INSERT INTO User(UserName, Password, PlayerId, Id) VALUES ('Marc', MD5('Marc'),(SELECT id from Player WHERE PlayerName = 'Marc'), 'D');
 
 INSERT INTO InsigniaModel(Name, Type) VALUES ('Welcome', '1');
 INSERT INTO InsigniaModel(Name, Type) VALUES ('Diamond', '2');
@@ -81,10 +79,6 @@ INSERT INTO InsigniaModel(Name, Type) VALUES ('Zombie', '4');
 INSERT INTO InsigniaModel(Name, Type) VALUES ('Scorched', '5');
 
 
-INSERT INTO Insignia(Id, Name, Data, PlayerId) VALUES ('1', 'Welcome',  '1/1/2000' , (SELECT id from Player WHERE PlayerName = 'Marc'));
-INSERT INTO Insignia(Id, Name, Data, PlayerId) VALUES ('2', 'Welcome',  '1/1/2000' , (SELECT id from Player WHERE PlayerName = 'Pau'));
-INSERT INTO Insignia(Id, Name, Data, PlayerId) VALUES ('3', 'Welcome',  '1/1/2000' , (SELECT id from Player WHERE PlayerName = 'Julia'));
-INSERT INTO Insignia(Id, Name, Data, PlayerId) VALUES ('4', 'Welcome',  '1/1/2000' , (SELECT id from Player WHERE PlayerName = 'Arnau'));
 
 
 INSERT INTO PlaneModel(Model, Fuel, EnginesLife, VelX, VelY, Gravity) VALUES ('Airbus', 60, 60, 70, 40, 100);
@@ -97,4 +91,3 @@ INSERT INTO PlaneModel(Model, Fuel, EnginesLife, VelX, VelY, Gravity) VALUES ('A
 INSERT INTO Plane(PlayerId, PlaneModelModel, Id) VALUES ((SELECT id from Player WHERE PlayerName = 'Arnau'), 'Cessna', '1');
 INSERT INTO Plane(PlayerId, PlaneModelModel, Id) VALUES ((SELECT id from Player WHERE PlayerName = 'Pau'), 'Cessna', '2');
 INSERT INTO Plane(PlayerId, PlaneModelModel, Id) VALUES ((SELECT id from Player WHERE PlayerName = 'Julia'), 'Cessna', '3');
-INSERT INTO Plane(PlayerId, PlaneModelModel, Id) VALUES ((SELECT id from Player WHERE PlayerName = 'Marc'), 'Cessna', '4');
