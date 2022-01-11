01b6132a7c73852f76650ff46ed1d85d7b35105e
mirarnau <arnau.mir@estudiantat.upc.edu>
2022-01-11 21:30:11 +0100

    Num Seq Millores

--- src/main/resources/userDB.sql
+++ src/main/resources/userDB.sql
@@ -72,8 +72,8 @@ CREATE TABLE Upgrade (
 CREATE TABLE ForumEntry (
                           Id varchar(255) PRIMARY KEY,
                           UserName varchar(255) NOT NULL,
-                          NumSeq integer NOT NULL,
-                          Message varchar(255) NOT NULL
+                          Message varchar(255) NOT NULL,
+                          NumSeq integer NOT NULL
 );
 
 
