69a9a6d1e65a898e8f0b6f93af352fe69e169bea
Pau Baguer <pauinki@gmail.com>
2022-01-13 18:28:29 +0100

    sql script for random statistics

---
+++ src/main/resources/randStats.sql
@@ -0,0 +1,13 @@
+Use insignia;
+
+update Player set MaxDistance = (Select ceiling(rand()*100)) where True;
+update Player set TimeOfFlight = (Select ceiling(rand()*100)) where True;
+update Player set Bitcoins = (Select ceiling(rand()*100)) where True;
+
+
+update Player set Rol = 'Second Officer' where Id = 'IdArnau';
+update Player set Rol = 'Senior First Officer' where Id = 'IdJulia';
+
+update Player set Rol = 'First Officer' where Id = 'IdMarc';
+
+update Player set Rol = 'Training Captain' where Id = 'IdPau';
